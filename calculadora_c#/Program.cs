c10e2ec87d6db98b75cafd66bd3b5be76c4816a7
Ana Julia <anajcaetanom@gmail.com>
2024-09-22 00:20:56 -0300

    Inicial commit

---
+++ calculadora_c#/Program.cs
@@ -0,0 +1,26 @@
+﻿string options = "1. Addition\n" +
+                 "2. Subtraction\n" +
+                 "3. Multiplication\n" +
+                 "4. Division\n";
+
+
+Console.WriteLine("✦•······• C# CALCULATOR •······•✦\n");
+
+Console.WriteLine(options);
+
+Console.Write("Type a number to choose an option: ");
+
+string chosen_option = Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
